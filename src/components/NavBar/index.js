d917c964ffff84b1b4edbd6180750e7fe21ec955
strongbubble <1303929162@qq.com>
2018-05-04 18:01:33 +0800

    添加NavBar组件

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SearchBar} from 'antd-mobile';
+import styles from './index.scss';
+
+
+class NavBartop extends React.Component{
+  render(){
+    const { leftContent, rightContent } = this.props;
+    return(
+    <div className={styles.navbar}>
+      <div className={styles.navbarleft}>{leftContent}</div>
+      <div className={styles.navbarconten} style={ !leftContent ? {width:'90%'} : {width:'80%'}}>
+        <SearchBar placeholder="请输入关键字搜索" />
+      </div>
+      <div className={styles.navbarright}>{rightContent}</div>
+    </div>
+    );
+  }
+}
+export default NavBartop;
