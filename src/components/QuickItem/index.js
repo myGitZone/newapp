e94e1cd5802da5dc07b62d455b93401b892d8bf5
18514253911 <513894105@qq.com>
2018-05-05 10:55:00 +0800

    框架优化，添加eslint和热更新

---
+++ src/components/QuickItem/index.js
@@ -0,0 +1,16 @@
+/**
+ * @author ycl
+ * @date 2018/5/4
+ * @Description:
+ */
+import React from 'react';
+
+class QuickItem extends React.PureComponent {
+  render() {
+    return (
+      <div>QuickItem</div>
+    );
+  }
+}
+
+export default QuickItem;
