59b706817436dcbdead927c8e1dc24990af0a404
strongbubble <1303929162@qq.com>
2018-05-05 17:30:11 +0800

    修改Navbar样式

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,7 +9,7 @@ class NavBartop extends React.Component {
     return (
       <div className={styles.navbar}>
         <div className={styles.navbarleft}>{leftContent}</div>
-        <div className={styles.navbarconten} style={!leftContent ? { width: '90%' } : { width: '80%' }}>
+        <div className={styles.navbarconten}>
           <SearchBar placeholder="请输入关键字搜索" />
         </div>
         <div className={styles.navbarright}>{rightContent}</div>
