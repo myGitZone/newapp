8c8b387d1fec5eaf1b6d95504901a4a8de0ba85d
18514253911 <513894105@qq.com>
2018-05-06 11:29:01 +0800

    完成部分首页功能

---
+++ src/components/Dynamic/index.js
@@ -0,0 +1,16 @@
+/**
+ * @author ycl
+ * @date 2018/5/6
+ * @Description:
+ */
+import React from 'react';
+
+class Dynamic extends React.PureComponent {
+  render() {
+    return (
+      <div>Dynamic</div>
+    );
+  }
+}
+
+export default Dynamic;
---
+++ src/components/OrganizationItem/index.js
@@ -0,0 +1,16 @@
+/**
+ * @author ycl
+ * @date 2018/5/5
+ * @Description:
+ */
+import React from 'react';
+
+class OrganizationItem extends React.PureComponent {
+  render() {
+    return (
+      <div>OrganizationItem</div>
+    );
+  }
+}
+
+export default OrganizationItem;
