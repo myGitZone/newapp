137c7d8a674c96cbbadbc626b39c98d6b2fab9cf
18514253911 <513894105@qq.com>
2018-05-06 19:01:14 +0800

    添加首页律师推荐

---
+++ src/components/Recommend/index.js
@@ -0,0 +1,16 @@
+/**
+ * @author ycl
+ * @date 2018/5/6
+ * @Description:
+ */
+import React from 'react';
+
+class Recommend extends React.PureComponent {
+  render() {
+    return (
+      <div>Recommend</div>
+    );
+  }
+}
+
+export default Recommend;
